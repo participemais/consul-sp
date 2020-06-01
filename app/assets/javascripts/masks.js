26b0c33f601a9a1581a7e967d1e181eaade27f05
Thiago <tms1_tms2@hotmail.com>
2020-06-01 16:48:31 -0300

    Apply mask just on input elements

--- app/assets/javascripts/masks.js
+++ app/assets/javascripts/masks.js
@@ -14,9 +14,9 @@
         }
       };
 
-      $('.js-date-of-birth-mask').mask('C0/A0/B000', { translation });
-      $('.js-cpf-mask').mask('000.000.000-00');
-      $('.js-cep-mask').mask('00000-000');
+      $('input.js-date-of-birth-mask').mask('C0/A0/B000', { translation });
+      $('input.js-cpf-mask').mask('000.000.000-00');
+      $('input.js-cep-mask').mask('00000-000');
     }
   }
 }).call(this);
