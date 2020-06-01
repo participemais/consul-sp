16ef70beca9249394d32dea172fb2e3c5cfbafad
Thiago <tms1_tms2@hotmail.com>
2020-06-01 17:32:46 -0300

    Revert "Devise key"
    This reverts commit 0c47f55f76aec6fdc8d984316bfaade83ff58184.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = "b3be4a4e35e452d936df52c752a7b24556ceddb8a1ac30ff548bcbdc33bec9e9a3c89c95d078ff0a3f2cae4f1f417234f476f11e754174a5405cbec082a3b8bf"
+  config.secret_key = Rails.application.secrets.secret_key_base
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
