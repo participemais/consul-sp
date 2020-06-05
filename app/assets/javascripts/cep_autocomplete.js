609f62cb197ed322eadd2301fb7b5eaee403e512
Thiago <tms1_tms2@hotmail.com>
2020-06-05 14:46:15 -0300

    Validação para CEP inválido

--- app/assets/javascripts/cep_autocomplete.js
+++ app/assets/javascripts/cep_autocomplete.js
@@ -2,18 +2,35 @@
   'use strict';
   App.CEPAutocomplete = {
     initialize: () => {
+      function clear_address_fields() {
+        $('#account_home_address').val('');
+        $('#account_city').val('');
+        $('#account_uf').val('');
+      }
+
       $('#account_cep').blur((handler) => {
         const cep = handler.target.value.replace(/\D/g, '');
+        const $input = $(handler.target);
+        $input.removeClass('is-invalid-input');
 
         if (cep.length == 8) {
           const url = `https://viacep.com.br/ws/${cep}/json/`;
           fetch(url)
             .then(response => response.json())
             .then(data => {
-              $("#account_home_address").val(data.logradouro);
-              $("#account_city").val(data.localidade);
-              $("#account_uf").val(data.uf);
+              if (!data.erro) {
+                $('#account_home_address').val(data.logradouro);
+                $('#account_city').val(data.localidade);
+                $('#account_uf').val(data.uf);
+              } else {
+                $input.addClass('is-invalid-input');
+                clear_address_fields();
+                alert('CEP inválido');
+              }
             });
+        } else {
+          $input.addClass('is-invalid-input');
+          clear_address_fields();
         }
 
       })
