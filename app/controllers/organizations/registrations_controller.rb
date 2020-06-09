7abbe92c4fe3255c07157932e4e4c6b003507f7a
Thiago <tms1_tms2@hotmail.com>
2020-06-09 09:19:57 -0300

    Remove campo de telefone do cadastro de organização

--- app/controllers/organizations/registrations_controller.rb
+++ app/controllers/organizations/registrations_controller.rb
@@ -30,7 +30,7 @@ class Organizations::RegistrationsController < Devise::RegistrationsController
   private
 
     def sign_up_params
-      params.require(:user).permit(:email, :password, :phone_number, :password_confirmation, :terms_of_service,
+      params.require(:user).permit(:email, :password, :password_confirmation, :terms_of_service,
                                    organization_attributes: [:name, :responsible_name])
     end
 
