b09966317cbc2c30af08c3de16d4f77d9a01d935
Thiago <tms1_tms2@hotmail.com>
2020-06-27 14:08:57 -0300

    Toggle js informações sobre distritos

--- app/assets/javascripts/district_data.js
+++ app/assets/javascripts/district_data.js
@@ -1,15 +1,16 @@
 (() => {
   'use strict';
   App.DistrictData = {
+    toggle_info: (handler) => {
+      const target = handler.target;
+      const id = target.dataset.district;
+      $(target).toggleClass("icon-plus-square icon-minus-square");
+      $(`[data-target="budget_district_${id}"]`).slideToggle();
+    },
     initialize: () => {
-      $(".js-district-info").on('click', (handler) => {
-        const target = handler.target;
-        const id = target.dataset.district;
-
-        $(target).removeClass("icon-plus-square").addClass("icon-minus-square");
-
-        $(`[data-target="budget_district_${id}"]`).slideDown(1000);
-      })
+      $(".js-district-info").on("click", (handler) => {
+        App.DistrictData.toggle_info(handler);
+      });
     }
   }
 }).call(this);
