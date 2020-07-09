a5e32fd3d0d1b80500cb519cc8b86a3e7228be6f
Thiago <tms1_tms2@hotmail.com>
2020-07-09 12:02:38 -0300

    Remove implementação de recaptcha

--- app/controllers/organizations/registrations_controller.rb
+++ app/controllers/organizations/registrations_controller.rb
@@ -1,6 +1,5 @@
 class Organizations::RegistrationsController < Devise::RegistrationsController
   invisible_captcha only: [:create], honeypot: :address, scope: :user
-  # prepend_before_action :check_captcha, only: [:create]
 
   def new
     super(&:build_organization)
@@ -32,16 +31,5 @@ class Organizations::RegistrationsController < Devise::RegistrationsController
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation,
                                    organization_attributes: [:name, :responsible_name])
-    end
-
-    def check_captcha
-      unless verify_recaptcha(timeout: 60)
-        flash.delete(:recaptcha_error)
-        build_resource(sign_up_params)
-        resource.valid?
-        resource.errors.add(:base, t('errors.messages.recaptcha_error'))
-        clean_up_passwords(resource)
-        respond_with_navigational(resource) { render :new }
-      end
     end
 end
