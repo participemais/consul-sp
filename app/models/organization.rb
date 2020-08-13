58bc73abe7dbb6ade4f272ff4864a7d84aa7f0e7
Thiago <tms1_tms2@hotmail.com>
2020-08-12 23:12:38 -0300

    Validação para responsável pela organização

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -10,6 +10,7 @@ class Organization < ApplicationRecord
   validates :responsible_name, length: { minimum: 2 }
   validate  :validate_responsible_name_length
   validate  :responsible_name_chars_validation
+  validate  :responsible_full_name
 
   before_save :sanitaze_responsible_name
 
@@ -72,6 +73,17 @@ class Organization < ApplicationRecord
     def responsible_name_chars_validation
       unless responsible_name =~ /^[\sa-z\u00C0-\u017F\.\-]+$/i
         errors.add(:responsible_name)
+      end
+    end
+
+    def responsible_full_name
+      words = responsible_name.split
+      if words.size < 2 || words.values_at(0, -1).any? { |word| word.size < 2 }
+        message = I18n.t(
+          :full_name_required,
+          scope: 'activerecord.errors.models.organization.attributes.responsible_name'
+        )
+        errors.add(:responsible_name, message)
       end
     end
 
