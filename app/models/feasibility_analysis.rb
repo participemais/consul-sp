629dce12d9e3562c7821f8843a0d8fdfd7dfea8a
Thiago <tms1_tms2@hotmail.com>
2020-08-20 09:21:23 -0300

    Campo de secretaria responsável como select

--- app/models/feasibility_analysis.rb
+++ app/models/feasibility_analysis.rb
@@ -3,4 +3,27 @@ class FeasibilityAnalysis < ApplicationRecord
   validates :feasibility_analyzable, presence: true
   validates :responsible, presence: true
   validates :responsible, uniqueness: { scope: :feasibility_analyzable_id }
+
+  AGENCIES = [
+    "Secretaria Municipal de Direitos Humanos e Cidadania",
+    "Secretaria Municipal de Educação",
+    "Secretaria Municipal de Esportes e Lazer",
+    "Secretaria Municipal de Fazenda",
+    "Secretaria Municipal de Gestão",
+    "Secretaria Municipal de Governo",
+    "Secretaria Municipal de Habitação",
+    "Secretaria Municipal de Infraestrutura e Obras",
+    "Secretaria Municipal de Inovação e Tecnologia",
+    "Secretaria Municipal de Justiça",
+    "Secretaria Municipal de Licenciamento",
+    "Secretaria Municipal de Mobilidade e Transportes",
+    "Secretaria Municipal da Pessoa com Deficiência",
+    "Procuradoria Geral do Município",
+    "Secretaria Municipal de Relações Sociais",
+    "Secretaria Municipal da Saúde",
+    "Secretaria Municipal de Segurança Urbana",
+    "Secretaria Municipal de Subprefeituras",
+    "Secretaria Municipal de Turismo",
+    "Secretaria Municipal do Verde e do Meio Ambiente"
+  ]
 end
