2c7e2ed5449396d6661f2105c986fb90b109ae43
Thiago <tms1_tms2@hotmail.com>
2020-08-25 13:58:20 -0300

    Escopo para secretaria ativas e ordenação

--- app/models/feasibility_analysis/department.rb
+++ app/models/feasibility_analysis/department.rb
@@ -2,4 +2,6 @@ class FeasibilityAnalysis::Department < ApplicationRecord
   has_many :feasibility_analyses
   validates :name, presence: true
   validates :name, uniqueness: true
+  scope :active, -> { where(active: true) }
+  scope :sort_by_name, -> { order(name: :asc) }
 end
