ed82c256888cbf23bc6d93ed980fc19370bf097c
Thiago <tms1_tms2@hotmail.com>
2020-08-26 11:10:40 -0300

    Mostra e esconde campos de viabilidade

--- app/assets/javascripts/valuation_budget_investment_form.js
+++ app/assets/javascripts/valuation_budget_investment_form.js
@@ -1,32 +1,35 @@
 (function() {
   "use strict";
   App.ValuationBudgetInvestmentForm = {
-    showFeasibleFields: function() {
-      $("#valuation_budget_investment_edit_form .feasible_fields").show();
+    hideAllFields: function() {
+      $("#valuation_budget_investment_edit_form .feasible_fields").hide("down");
+      $("#valuation_budget_investment_edit_form .unfeasible_fields").hide("down");
     },
     showNotFeasibleFields: function() {
       $("#valuation_budget_investment_edit_form .feasible_fields").hide("down");
+      $("#valuation_budget_investment_edit_form .unfeasible_fields").show("down");
     },
     showAllFields: function() {
       $("#valuation_budget_investment_edit_form .feasible_fields").show("down");
+      $("#valuation_budget_investment_edit_form .unfeasible_fields").show("down");
     },
     showFeasibilityFields: function() {
       var feasibility;
       feasibility = $("#valuation_budget_investment_edit_form input[type=radio][name='budget_investment[feasibility]']:checked").val();
       if (feasibility === "feasible") {
-        App.ValuationBudgetInvestmentForm.showFeasibleFields();
+        App.ValuationBudgetInvestmentForm.showAllFields();
       } else if (feasibility === "unfeasible") {
         App.ValuationBudgetInvestmentForm.showNotFeasibleFields();
+      } else {
+        App.ValuationBudgetInvestmentForm.hideAllFields();
       }
     },
     showFeasibilityFieldsOnChange: function() {
       $("#valuation_budget_investment_edit_form input[type=radio][name='budget_investment[feasibility]']").on("change", function() {
-        App.ValuationBudgetInvestmentForm.showAllFields();
         App.ValuationBudgetInvestmentForm.showFeasibilityFields();
       });
     },
     initialize: function() {
-      App.ValuationBudgetInvestmentForm.showFeasibilityFields();
       App.ValuationBudgetInvestmentForm.showFeasibilityFieldsOnChange();
     }
   };
