e8a30e1b8b6bef4cfe6ae95b260de309f6382205
Thiago <tms1_tms2@hotmail.com>
2020-08-26 16:38:04 -0300

    Remove nomes das secretarias

--- app/models/feasibility_analysis.rb
+++ app/models/feasibility_analysis.rb
@@ -16,27 +16,4 @@ class FeasibilityAnalysis < ApplicationRecord
   def should_show_codes?
     sei_number.present? || budgetary_actions.present?
   end
-
-  AGENCIES = [
-    "Secretaria Municipal de Direitos Humanos e Cidadania",
-    "Secretaria Municipal de Educação",
-    "Secretaria Municipal de Esportes e Lazer",
-    "Secretaria Municipal de Fazenda",
-    "Secretaria Municipal de Gestão",
-    "Secretaria Municipal de Governo",
-    "Secretaria Municipal de Habitação",
-    "Secretaria Municipal de Infraestrutura e Obras",
-    "Secretaria Municipal de Inovação e Tecnologia",
-    "Secretaria Municipal de Justiça",
-    "Secretaria Municipal de Licenciamento",
-    "Secretaria Municipal de Mobilidade e Transportes",
-    "Secretaria Municipal da Pessoa com Deficiência",
-    "Procuradoria Geral do Município",
-    "Secretaria Municipal de Relações Sociais",
-    "Secretaria Municipal da Saúde",
-    "Secretaria Municipal de Segurança Urbana",
-    "Secretaria Municipal de Subprefeituras",
-    "Secretaria Municipal de Turismo",
-    "Secretaria Municipal do Verde e do Meio Ambiente"
-  ]
 end
