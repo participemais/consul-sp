43d78f745bcb09d4aabadb1c6915ea31ba72d101
Thiago <tms1_tms2@hotmail.com>
2020-09-09 09:14:21 -0300

    Rotas para topic e topic levels

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -216,6 +216,8 @@ namespace :admin do
       resources :milestones
       resources :progress_bars, except: :show
       resource :homepage, only: [:edit, :update]
+      resources :topics, except: [:show]
+      resources :topic_levels, except: [:index, :show]
     end
   end
 
--- config/routes/legislation.rb
+++ config/routes/legislation.rb
@@ -7,6 +7,7 @@ namespace :legislation do
       get :result_publication
       get :proposals
       get :milestones
+      get :topics
     end
 
     resources :questions, only: [:show] do
