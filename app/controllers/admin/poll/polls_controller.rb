e76c5df368786e727de38f29770bca156ccf0c7c
Thiago <tms1_tms2@hotmail.com>
2020-10-20 16:12:12 -0300

    Checkbox para restrito por colégio eleitoral no formulário de votação

--- app/controllers/admin/poll/polls_controller.rb
+++ app/controllers/admin/poll/polls_controller.rb
@@ -77,6 +77,7 @@ class Admin::Poll::PollsController < Admin::Poll::BaseController
 
     def poll_params
       attributes = [:name, :starts_at, :ends_at, :geozone_restricted, :budget_id,
+                    :electoral_college_restricted,
                     geozone_ids: [], image_attributes: image_attributes]
 
       params.require(:poll).permit(*attributes, *report_attributes, translation_params(Poll))
