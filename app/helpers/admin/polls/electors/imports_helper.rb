ff19419c49cf48e9f0e8be6c235e928c6a50ee61
Thiago <tms1_tms2@hotmail.com>
2020-10-22 12:28:06 -0300

    Traduções para o new de import

---
+++ app/helpers/admin/polls/electors/imports_helper.rb
@@ -0,0 +1,13 @@
+module Admin::Polls::Electors::ImportsHelper
+  def errors_for(resource, field)
+    if resource.errors.include? field
+      tag.div class: "error" do
+        resource.errors[field].join(", ")
+      end
+    end
+  end
+
+  def electors_import_required_headers
+    Poll::Elector::Import::ATTRIBUTES.to_sentence(last_word_connector: " e ")
+  end
+end
