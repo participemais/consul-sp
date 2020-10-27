e273ceb5ca3d52fc692c8ce3da9f77356a2ad209
Thiago <tms1_tms2@hotmail.com>
2020-10-27 12:05:15 -0300

    Agenda desativação do colégio eleitoral

--- app/models/poll/electoral_college.rb
+++ app/models/poll/electoral_college.rb
@@ -6,8 +6,22 @@ class Poll
       foreign_key: "poll_electoral_college_id",
       dependent: :destroy
 
+    after_save :schedule_electoral_college_deactivation
+
     def filename
       title.parameterize
+    end
+
+    def deactivate_electoral_college
+      self.update(active: false)
+    end
+
+    private
+
+    def schedule_electoral_college_deactivation
+      self.delay(
+        priority: 10, run_at: poll.ends_at.end_of_day, queue: "schedules"
+      ).deactivate_electoral_college
     end
   end
 end
