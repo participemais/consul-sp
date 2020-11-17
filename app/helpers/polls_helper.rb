08ed4c5618c2b0d2cb984d76597e1df192c42f7a
Thiago <tms1_tms2@hotmail.com>
2020-11-17 13:50:49 -0300

    Arruma contagem total de votos para mostrar votos presenciais só depois do final da votação

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -79,4 +79,8 @@ module PollsHelper
     return "" if index.even?
     index % 3 == 0 ? "" : "answer-info-border-right"
   end
+
+  def poll_total_votes_count
+    @poll.expired? ? @poll.answer_count : @poll.web_answers_count
+  end
 end
