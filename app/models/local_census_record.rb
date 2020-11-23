11504aa67a33d2969932fea750c3dba6ab96046e
Thiago <tms1_tms2@hotmail.com>
2020-11-23 13:34:01 -0300

    Acrescenta campos de raça e gênero no formulário de local census record

--- app/models/local_census_record.rb
+++ app/models/local_census_record.rb
@@ -1,19 +1,24 @@
 class LocalCensusRecord < ApplicationRecord
   before_validation :sanitize
+  before_save :sanitize_document_type
 
   validates :document_number, presence: true
   validates :document_type, presence: true
-  validates :document_type, inclusion: { in: ["1", "2", "3"], allow_blank: true }
+  validates :document_type, inclusion: { in: User.document_type_options }
   validates :date_of_birth, presence: true
   validates :postal_code, presence: true
-  validates :document_number, uniqueness: { scope: :document_type }
+  validates :document_number, uniqueness: true
 
   scope :search, ->(terms) { where("document_number ILIKE ?", "%#{terms}%") }
 
   private
 
+    def sanitize_document_type
+      self.document_type   = self.document_type.downcase
+    end
+
     def sanitize
-      self.document_type   = self.document_type&.strip
+      self.document_type   = self.document_type&.upcase&.strip
       self.document_number = self.document_number&.strip
       self.postal_code     = self.postal_code&.strip
     end
