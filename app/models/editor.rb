d805f516fe8bc6963e1089d107ec96f43642752d
Julian Gutierrez <juliansouza@gmail.com>
2020-11-25 15:53:09 -0300

    Adiciona colunas na tabela de editores

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -1,6 +1,6 @@
 class Editor < ApplicationRecord
-	has_many :editor_legislation_processes
-	has_many :legislation_processes, through: :editor_legislation_processes
+	has_many :editor_legislation_processes, foreign_key: "editor_id"
+	has_many :legislation_processes, through: :editor_legislation_processes, source: :process
 
   belongs_to :user, touch: true
   delegate :name, :email, :name_and_email, to: :user, allow_nil: true
