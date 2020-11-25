ff9076c64d10f9baba515ccdf06f9e3b916ea104
Thiago <tms1_tms2@hotmail.com>
2020-11-25 11:27:31 -0300

    Campo para explicar diferença entre votantes e cédulas, se houver

--- app/models/poll/recount.rb
+++ app/models/poll/recount.rb
@@ -5,8 +5,11 @@ class Poll::Recount < ApplicationRecord
   belongs_to :booth_assignment
   belongs_to :officer_assignment
 
+  has_many :voters, through: :booth_assignment
+
   validates :author, presence: true
   validates :origin, inclusion: { in: VALID_ORIGINS }
+  validate :match_voters_and_ballots
 
   scope :web,    -> { where(origin: "web") }
   scope :booth,  -> { where(origin: "booth") }
@@ -32,5 +35,12 @@ class Poll::Recount < ApplicationRecord
   def update_officer_author
     self.officer_assignment_id_log += ":#{officer_assignment_id_in_database}"
     self.author_id_log += ":#{author_id_in_database}"
+  end
+
+  def match_voters_and_ballots
+    return if voters.empty?
+    if total_amount > voters.size && difference_explanation.blank?
+      errors.add(:total_amount)
+    end
   end
 end
