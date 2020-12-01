919c50337b8efd7d210f3827371ab9bbcb81ffa7
Thiago <tms1_tms2@hotmail.com>
2020-11-30 22:28:29 -0300

    Padronização para formato de arquivo

--- app/models/document.rb
+++ app/models/document.rb
@@ -70,10 +70,24 @@ class Document < ApplicationRecord
   end
 
   def humanized_content_type
-    attachment_content_type.split("/").last.upcase
+    content_type = attachment_content_type.split("/").last
+
+    if type = CONTENT_TYPE_PATTERN[content_type.to_sym]
+      type
+    else
+      content_type.upcase
+    end
   end
 
   private
+
+    CONTENT_TYPE_PATTERN = {
+      "msword": "DOC",
+      "plain": "CSV",
+      "vnd.openxmlformats-officedocument.wordprocessingml.document": "DOCX",
+      "vnd.openxmlformats-officedocument.spreadsheetml.sheet": "XLSX",
+      "x-ole-storage": "XLS"
+    }.freeze
 
     def documentable_class
       documentable_type.constantize if documentable_type.present?
