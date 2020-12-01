7fbea6513611fc77a980aeada37efa8c0b3ac7fd
Thiago <tms1_tms2@hotmail.com>
2020-12-01 20:46:21 -0300

    Remove coluna de descrição para fase elaboração do PLOA

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201111193449) do
+ActiveRecord::Schema.define(version: 20201201232213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -391,7 +391,6 @@ ActiveRecord::Schema.define(version: 20201111193449) do
     t.text "description_informing"
     t.integer "max_votes"
     t.string "balloting_type"
-    t.text "description_formulation"
     t.text "description_devolutive"
     t.string "voting_style", default: "knapsack"
   end
