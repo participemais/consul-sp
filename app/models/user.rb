2246eaf5753ec28c9396bd011a0e1068d20bb233
Thiago <tms1_tms2@hotmail.com>
2020-12-01 21:54:56 -0300

    Validação de data de nascimento

--- app/models/user.rb
+++ app/models/user.rb
@@ -496,7 +496,7 @@ class User < ApplicationRecord
           scope: 'activerecord.errors.models.user.attributes.date_of_birth',
           age: min_age
         )
-        errors.add(:base, message)
+        errors.add(:date_of_birth, message)
       end
     end
 
