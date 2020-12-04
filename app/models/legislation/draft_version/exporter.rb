ad04b72d9cf742b25ab9b2952d299621ba1e492f
Maria Luciene <maria_luciene12@hotmail.com>
2020-12-04 14:55:21 -0300

    CSV de anotacoes - Mensagem informando quando nao possui anotaçoes

--- app/models/legislation/draft_version/exporter.rb
+++ app/models/legislation/draft_version/exporter.rb
@@ -9,20 +9,28 @@ class Legislation::DraftVersion::Exporter
 
   def to_csv
     CSV.generate(headers: true) do |csv|
-      csv << ANNOTATION_COLUMNS.map { |column| header_translation(column) }
 
-      @draft_version.annotations.each do |annotation|
-        csv << csv_row(@draft_version, annotation)
+      if @draft_version.annotations.empty?
+        csv << NO_ANNOTATION_COLUMNS.map { |column| header_translation(column) }
+        csv << csv_row(@draft_version)
+      else
+        csv << ANNOTATION_COLUMNS.map { |column| header_translation(column) }
+
+        @draft_version.annotations.each do |annotation|
+          csv << annotation_csv_row(@draft_version, annotation)
+        end
       end
     end
   end
 
   private
 
+
   ANNOTATION_COLUMNS = %w(id title text comment user).freeze
+  NO_ANNOTATION_COLUMNS = %w(id title comment).freeze
 
 
-  def csv_row(draft_version, annotation)
+  def annotation_csv_row(draft_version, annotation)
     [
       draft_version.id.to_s,
       draft_version.title,
@@ -30,6 +38,18 @@ class Legislation::DraftVersion::Exporter
       annotation.text,
       annotation.author.name
     ]
+  end
+
+    def csv_row(draft_version)
+    [
+      draft_version.id.to_s,
+      draft_version.title,
+      no_comments,
+    ]
+  end
+
+  def no_comments
+    I18n.t("legislation.draft_versions.spreedsheet.no_comments")
   end
 
   def header_translation(key)
