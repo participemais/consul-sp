bcfa293de6d4acb597af96026f4cb1589e16ac8f
Thiago <tms1_tms2@hotmail.com>
2020-12-07 14:04:51 -0300

    Revert "Remove coluna de descrição para fase elaboração do PLOA"
    This reverts commit 7fbea6513611fc77a980aeada37efa8c0b3ac7fd.

--- db/migrate/20201201232213_remove_formulation_description_from_budgets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFormulationDescriptionFromBudgets < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :budgets, :description_formulation, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201201232213) do
+ActiveRecord::Schema.define(version: 20201111193449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -391,6 +391,7 @@ ActiveRecord::Schema.define(version: 20201201232213) do
     t.text "description_informing"
     t.integer "max_votes"
     t.string "balloting_type"
+    t.text "description_formulation"
     t.text "description_devolutive"
     t.string "voting_style", default: "knapsack"
   end
