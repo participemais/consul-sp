95c316bcb777a95b0cf9864928b46ec1ef511a0d
Thiago <tms1_tms2@hotmail.com>
2020-12-20 17:00:11 -0300

    Controller para governo aberto

---
+++ app/controllers/admin/open_gov/articles_controller.rb
@@ -0,0 +1,27 @@
+class Admin::OpenGov::ArticlesController < Admin::BaseController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
