afd87178fb8a5a396c135b9882ffa0f1faa6ba83
Thiago <tms1_tms2@hotmail.com>
2020-12-20 22:14:43 -0300

    CRUD de Article

--- app/controllers/admin/open_gov/articles_controller.rb
+++ app/controllers/admin/open_gov/articles_controller.rb
@@ -1,27 +1,50 @@
 class Admin::OpenGov::ArticlesController < Admin::BaseController
 
+  before_action :load_article, only: [:show, :edit, :update, :destroy]
+
   def index
-
-  end
-
-  def show
-
+    @articles = OpenGov::Article.all
   end
 
   def new
-
+    @article = OpenGov::Article.new
   end
 
   def create
+    @article = OpenGov::Article.new(article_params)
+    @article.author_id = current_user.id
 
+    if @article.save
+      redirect_to admin_open_gov_article_path(@article),
+        notice: t("admin.open_gov.articles.create.notice")
+    else
+      render :new
+    end
   end
 
   def update
-
+    if @article.update(article_params)
+      redirect_to admin_open_gov_article_path(@article),
+        notice: t("admin.open_gov.articles.update.notice")
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to admin_open_gov_articles_path,
+      notice: t("admin.open_gov.articles.delete.notice")
+  end
 
+  private
+
+  def article_params
+    params.require(:open_gov_article).permit(:title, :text)
+  end
+
+  def load_article
+    @article = OpenGov::Article.find(params[:id])
   end
 
 end
---
+++ app/helpers/open_gov_helper.rb
@@ -0,0 +1,7 @@
+module OpenGovHelper
+  def open_gov_tabs
+    {
+      "articles" => admin_open_gov_articles_path
+    }
+  end
+end
