b5734f7036a9d93d6a4bfdcff1e337c713eb393f
Maria Luciene <maria_luciene12@hotmail.com>
2021-02-21 11:43:18 -0300

    Envia emails de propostas selecionadas e nao selecionadas

--- app/controllers/admin/budgets_controller.rb
+++ app/controllers/admin/budgets_controller.rb
@@ -37,6 +37,8 @@ class Admin::BudgetsController < Admin::BaseController
   def update
     if @budget.update(budget_params)
 
+      send_selected_and_unselected_emails if @budget.balloting?
+
       redirect_to admin_budgets_path, notice: t("admin.budgets.update.notice")
     else
       load_staff
@@ -85,5 +87,10 @@ class Admin::BudgetsController < Admin::BaseController
     def load_staff
       @admins = Administrator.includes(:user)
       @valuators = Valuator.includes(:user).order(description: :asc).order("users.email ASC")
+    end
+
+    def send_selected_and_unselected_emails
+      @budget.email_selected
+      @budget.email_unselected
     end
 end
